996f282b7a777ba9cc0e33a8b0be62c2ac11f930
aneperez <127619963+aneperez@users.noreply.github.com>
2024-05-04 17:16:12 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,78 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, View } from 'react-native';
+import NomeQualquer from './assets/viaCepLogo.jpg'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <Image
+        style={{position: 'absolute', top: 40}}
+        source={NomeQualquer}
+      />
+
+      <View 
+        style={styles.containerInfos}
+      >
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}
+  containerInfos: {
+    borderWidth: 2,
+    width: '100%'
+    height:'60%'
+
+
+    <TextImput>
+        //keyboardType='numeric'
+        placeholder='Ane'
+        style={styles.imputStyle}
+        />
+
+
+        <TextImput>
+        //keyboardType='numeric'
+        placeholder='Ane'
+        style={styles.imputStyle}
+        />
+
+        <TextImput>
+        //keyboardType='numeric'
+        placeholder='Ane'
+        style={styles.imputStyle}
+        />
+
+        <TextImput>
+        //keyboardType='numeric'
+        placeholder='Ane'
+        style={styles.imputStyle}
+        />
+
+        <TextImput>
+        //keyboardType='numeric'
+        placeholder='Ane'
+        style={styles.imputStyle}
+        />
+
+{
+
+const styles = StyleSheet.create({
+    
+})
+}
+
+
+
+  })
